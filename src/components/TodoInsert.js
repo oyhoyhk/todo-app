13c9350537b6ddb25d549412001b23bc5c6ec7ee
oyhoyhk <oyhoyhk@naver.com>
2019-11-25 18:17:18 +0900

    todo

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MdAnnouncement } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = () => {
+  return (
+    <form className="TodoInsert">
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit">
+        <MdAnnouncement />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
